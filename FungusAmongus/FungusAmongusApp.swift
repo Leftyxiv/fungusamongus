b6c9023bedb7a58392cfec9ebe704ae7fc0c43f0
manny ledoux <manny.ledoux@checkr.com>
2024-09-14 07:08:36 -0400

    Initial Commit

---
+++ FungusAmongus/FungusAmongusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FungusAmongusApp.swift
+//  FungusAmongus
+//
+//  Created by manny ledoux on 9/14/24.
+//
+
+import SwiftUI
+
+@main
+struct FungusAmongusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
